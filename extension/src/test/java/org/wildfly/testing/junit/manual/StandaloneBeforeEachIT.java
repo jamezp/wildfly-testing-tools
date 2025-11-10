a59249f0a7571db4917a835eaeaa18dda7200900
James R. Perkins <jperkins@ibm.com>
2025-11-10 10:53:48 -0800

    Create a new annotation for manual mode tests and add support for manual mode testing.

---
+++ extension/src/test/java/org/wildfly/testing/junit/manual/StandaloneBeforeEachIT.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright The WildFly Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.wildfly.testing.junit.manual;
+
+/**
+ *
+ * @author <a href="mailto:jperkins@ibm.com">James R. Perkins</a>
+ */
+public class StandaloneBeforeEachIT extends AbstractBeforeEach {
+    @Override
+    public String expectedLaunchType() {
+        return "STANDALONE";
+    }
+}
